0ac07efc594b87da91834d24c5d2d87624b4de6c
andrea-lopez2 <143145183+andrea-lopez2@users.noreply.github.com>
2023-09-17 00:00:19 +0000

    03 Homework

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -8,9 +8,10 @@ using std::cin;
 
 int main() 
 {
+	
 	int choice;
 	cout << "1) Get letter grade using if. " << endl;
-	cout << "2) Get letter grade using swittch. " << endl;
+	cout << "2) Get letter grade using switch. " << endl;
 	cout << "3) Exit " << endl;
 	cin >> choice;
 
@@ -24,12 +25,12 @@ int main()
 
 		if(grade < 0 || grade > 100)
 		{
-			cout << "  The grade you entered is not in range (0 - 100). ";
+			cout << "  The grade you entered is not in range (0 - 100). \n";
 		}
 		else if(grade >= 0 && grade <= 100)
 		{
 			string letter = get_letter_grade_using_if(grade);
-			cout << "You got a -> " << letter << endl;
+			cout << "(IF) You got a -> " << letter << endl;
 		}
 	}
 	else if(choice == 2)
@@ -40,12 +41,12 @@ int main()
 
 		if(grade < 0 || grade > 100)
 		{
-			cout << "  The grade you entered is not in range (0 - 100). ";
+			cout << "  The grade you entered is not in range (0 - 100). \n";
 		}
 		else 
 		{
 			string letter = get_letter_grade_using_switch(grade);
-			cout << "You got a -> " << letter << endl;
+			cout << "(SWITCH) You got a -> " << letter << endl;
 		}
 	}
 
--- test/homework/03_decisions/03_decisions_tests.cpp
+++ test/homework/03_decisions/03_decisions_tests.cpp
@@ -7,3 +7,25 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Verify Grades using IF Statement") 
+{
+	REQUIRE(get_letter_grade_using_if(95) == "A");
+	REQUIRE(get_letter_grade_using_if(85) == "B");
+	REQUIRE(get_letter_grade_using_if(75) == "C");
+	REQUIRE(get_letter_grade_using_if(65) == "D");
+	REQUIRE(get_letter_grade_using_if(50) == "F");
+
+
+
+}
+
+
+TEST_CASE("Verify Grades using SWITCH Statement") {
+
+	REQUIRE(get_letter_grade_using_switch(95) == "A");
+	REQUIRE(get_letter_grade_using_switch(85) == "B");
+	REQUIRE(get_letter_grade_using_switch(75) == "C");
+	REQUIRE(get_letter_grade_using_switch(65) == "D");
+	REQUIRE(get_letter_grade_using_switch(50) == "F");
+
+}
